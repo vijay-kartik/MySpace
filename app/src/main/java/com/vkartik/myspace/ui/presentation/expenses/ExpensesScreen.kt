4dfd45718b729f6c6782b22005ce79b642ae1725
vijay-kartik <kartik.iit96@gmail.com>
2024-07-07 03:26:26 +0530

    Add ExpensesViewModel, set app bar title according to current subscreen

--- app/src/main/java/com/vkartik/myspace/ui/presentation/expenses/ExpensesScreen.kt
+++ app/src/main/java/com/vkartik/myspace/ui/presentation/expenses/ExpensesScreen.kt
@@ -2,8 +2,9 @@ package com.vkartik.myspace.ui.presentation.expenses
 
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.hilt.navigation.compose.hiltViewModel
 
 @Composable
-fun ExpensesScreen() {
-    Text("Expenses")
+fun ExpensesScreen(viewModel: ExpensesViewModel = hiltViewModel()) {
+    Text("Total Expense")
 }
